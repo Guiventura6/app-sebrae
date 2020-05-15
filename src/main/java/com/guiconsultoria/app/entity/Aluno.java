cc3d248a09f0d82cb6f922eb9558bc20cf8ccad5
Guilherme Ventura sousa <61128802+GuilhermeVenturaSousa@users.noreply.github.com>
2020-05-14 21:27:23 -0300

    Criacao da entidade Aluno e modificacao do curso

---
+++ src/main/java/com/guiconsultoria/app/entity/Aluno.java
@@ -0,0 +1,107 @@
+package com.guiconsultoria.app.entity;
+
+import java.io.Serializable;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.JoinColumn;
+import javax.persistence.OneToOne;
+import javax.persistence.Table;
+
+public class Aluno implements Serializable{
+	private static final long serialVersionUID = 1L;
+
+	@Entity
+	@Table(name = "tb_aluno")
+	public class Curso {
+
+		@Id
+		@GeneratedValue(strategy = GenerationType.AUTO)
+		@Column(name = "id_aluno", nullable = false)
+		private Long id;
+
+		@Column(name = "nome", nullable = false)
+		private String nome;
+
+		@Column(name = "rg", nullable = false)
+		private String rg;
+		
+		@Column(name = "cpf", nullable = false)
+		private String cpf;
+		
+		@Column(name = "email", nullable = false)
+		private String email;
+		
+		@Column(name = "data_nasc", nullable = false)
+		private Integer data_nasc;
+		
+		@OneToOne
+		@JoinColumn(name = "id_curso")
+		private Curso curso;
+
+		public Long getId() {
+			return id;
+		}
+
+		public void setId(Long id) {
+			this.id = id;
+		}
+
+		public String getNome() {
+			return nome;
+		}
+
+		public void setNome(String nome) {
+			this.nome = nome;
+		}
+
+		public String getRg() {
+			return rg;
+		}
+
+		public void setRg(String rg) {
+			this.rg = rg;
+		}
+
+		public String getCpf() {
+			return cpf;
+		}
+
+		public void setCpf(String cpf) {
+			this.cpf = cpf;
+		}
+
+		public String getEmail() {
+			return email;
+		}
+
+		public void setEmail(String email) {
+			this.email = email;
+		}
+
+		public Integer getData_nasc() {
+			return data_nasc;
+		}
+
+		public void setData_nasc(Integer data_nasc) {
+			this.data_nasc = data_nasc;
+		}
+
+		public Curso getCurso() {
+			return curso;
+		}
+
+		public void setCurso(Curso curso) {
+			this.curso = curso;
+		}
+
+		@Override
+		public String toString() {
+			return "Curso [id=" + id + ", nome=" + nome + ", rg=" + rg + ", cpf=" + cpf + ", email=" + email
+					+ ", data_nasc=" + data_nasc + ", curso=" + curso + "]";
+		}
+	}
+}
--- src/main/java/com/guiconsultoria/app/entity/Curso.java
+++ src/main/java/com/guiconsultoria/app/entity/Curso.java
@@ -1,15 +1,24 @@
 package com.guiconsultoria.app.entity;
 
+import java.io.Serializable;
+import java.util.ArrayList;
+import java.util.List;
+
 import javax.persistence.Column;
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
+import javax.persistence.MapsId;
+import javax.persistence.OneToOne;
 import javax.persistence.Table;
+
+import com.fasterxml.jackson.annotation.JsonIgnore;
 
 @Entity
 @Table(name = "tb_curso")
-public class Curso {
+public class Curso implements Serializable{
+	private static final long serialVersionUID = 1L;
 
 	@Id
 	@GeneratedValue(strategy = GenerationType.AUTO)
@@ -21,6 +30,11 @@ public class Curso {
 
 	@Column(name = "descricao", nullable = false)
 	private String descricao;
+	
+	@JsonIgnore
+	@OneToOne
+	@MapsId
+	private List<Aluno> orders = new ArrayList<>();
 
 	public Long getId() {
 		return id;
